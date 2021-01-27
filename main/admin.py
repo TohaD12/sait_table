2a862b98eb81ed4d8189900d7a6d3504a9dba46b
toha <smssobaka@gmail.com>
2021-01-27 21:40:49 +0300

    delete all

--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Task, Accessories
-
-
-admin.site.register(Task)
-admin.site.register(Accessories)
--- mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views import View
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-]
