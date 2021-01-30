7e043570ee841bef1a576f5bb8f73e6888c95282
toha <smssobaka@gmail.com>
2021-01-30 15:05:35 +0300

    овтоподсчет элементов таблицы + одвеременно остается возможность внести данные вручную

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,5 @@
 from .models import Task, Accessories
 from .forms import TaskForm, AccessoriesForm
-from django.db.models import Q
 from django.shortcuts import render
 
 
@@ -20,11 +19,34 @@ def about(request):
 
 
 def create(request):
-    
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+            obj = form.save(commit=False)
+            if obj.after_adjustment_quantity_actual == "":
+                obj.after_adjustment_quantity_actual = obj.quantity_actual
+            else:
+                obj.after_adjustment_quantity_actual = obj.after_adjustment_quantity_actual
+
+            if obj.after_adjustment_quantity_1c == "":
+                if obj.adjustments != "0" and obj.adjustments != "":
+                    obj.after_adjustment_quantity_1c = str(int(obj.quantity_1c) + int(obj.adjustments))
+                    if int(obj.adjustments) >= 1 and int(obj.delta) >= 1:
+                        obj.after_adjustment_delta = str(int(obj.delta) - int(obj.adjustments))
+                    elif int(obj.adjustments) <= -1 and int(obj.delta) <= -1:
+                        obj.after_adjustment_delta = str(int(obj.delta) - int(obj.adjustments))
+                    elif int(obj.adjustments) <= -1 and int(obj.delta) >= 1:
+                        obj.after_adjustment_delta = str(int(obj.delta) + int(obj.adjustments))
+                    elif int(obj.adjustments) >= 1 and int(obj.delta) <= -1:
+                        obj.after_adjustment_delta = str(int(obj.delta) + int(obj.adjustments))
+            else:
+                obj.after_adjustment_quantity_1c = obj.after_adjustment_quantity_1c
+
+            if obj.after_adjustment_delta == "":
+                obj.after_adjustment_delta = obj.delta
+            else:
+                obj.after_adjustment_delta = obj.after_adjustment_delta
+            obj.save()
 
     form = TaskForm()
     context = {
@@ -37,7 +59,23 @@ def create_accessories(request):
     if request.method == 'POST':
         form = AccessoriesForm(request.POST)
         if form.is_valid():
-            form.save()
+            obj = form.save(commit=False)
+            if obj.after_adjustment_quantity_actual == "":
+                obj.after_adjustment_quantity_actual = obj.quantity_actual
+            else:
+                obj.after_adjustment_quantity_actual = obj.after_adjustment_quantity_actual
+
+            if obj.after_adjustment_quantity_1c == "":
+                obj.after_adjustment_quantity_1c = obj.quantity_1c
+            else:
+                obj.after_adjustment_quantity_1c = obj.after_adjustment_quantity_1c
+
+            if obj.after_adjustment_delta == "":
+                obj.after_adjustment_delta = obj.delta
+            else:
+                obj.after_adjustment_delta = obj.after_adjustment_delta
+
+            obj.save()
 
     form = AccessoriesForm()
     context = {
@@ -73,4 +111,3 @@ def search_accessories(request):
     else:
         task = Accessories.objects.all()
     return render(request, 'main/search_accessories.html', {'details_name': 'Главная страница сайта', 'task': task})
-
---
+++ mysite/test.py
@@ -0,0 +1,11 @@
+a = "1"
+b = "-1"
+b = int(b)
+print(b)
+if int(b) <= -1 or int(a) >= 1:
+    c = str(int(b) + int(a))
+
+print(c)
+#
+# elif int(obj.adjustments) <= -1 or int(obj.delta) >= 1:
+# obj.after_adjustment_delta = str(int(obj.delta) + int(obj.adjustments))
